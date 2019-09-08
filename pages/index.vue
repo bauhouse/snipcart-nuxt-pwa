9f14119443a01879fd826270a1a4962089c9ad2b
Stephen Bau <bauhouse@gmail.com>
2019-09-08 16:59:05 -0700

    Modify index page template to import articles

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,47 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <card
-        title="Free"
-        icon="github-circle"
-      >
-        Open source on <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
-
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">
-          Every
-        </b> component is responsive
-      </card>
-
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">
-          Vue.js
-        </a> and <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
-
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
-        No other internal dependency
-      </card>
-    </div>
-  </section>
+  <div class="main">
+    <main>
+      <div>
+        <article class="post" v-for="(article, index) in articles" :key="index">
+          <div class="post-aside">
+            <small>{{ article.attributes.date }}</small>
+            <h3 class="post-title"><nuxt-link :to="article.attributes.link">{{ article.attributes.title }}</nuxt-link></h3>
+            <p>{{ article.attributes.description }}</p>
+          </div>
+          <div class="products">
+            <article v-for="(product, index) in article.attributes.products" :key="index">
+              <img :src="product.image" :alt="product.name">
+              <button
+                class="buy-button snipcart-add-item"
+                :data-item-id="product.sku"
+                :data-item-name="product.name"
+                :data-item-price="product.price"
+                :data-item-image="product.image"
+                :data-item-url="`https://snipcart-nuxt-pwa.netlify.com/`">
+                {{`$${product.price}`}}
+              </button>
+              <p class="product-name">{{product.name}}</p>
+            </article>
+          </div>
+        </article>
+      </div>
+      <Testimonals />
+    </main>
+  </div>
 </template>
 
 <script>
-import Card from '~/components/Card'
+import articles from '~/contents/journal/journal.js'
 
 export default {
-  name: 'HomePage',
-
-  components: {
-    Card
+  async asyncData ({ route }) {
+    const promises = articles.map(article => import(`~/contents/journal/${article}.md`))
+    return { articles: await Promise.all(promises) }
+  },
+  head() {
+    return {
+      title: "All posts | Nuxt.js PWA store"
+    }
   }
 }
 </script>
