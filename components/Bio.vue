d4a1421ac4db2207bdaa90a7f609fcfc42153371
Stephen Bau <bauhouse@gmail.com>
2019-09-08 16:49:21 -0700

    Add Bio component

---
+++ components/Bio.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bio">
+    <img v-if="this.$nuxt.$route.name !== 'index'"
+      src="~/static/icon.png"
+    />
+    <div>
+      <p>
+        Blog article edited by
+        <strong>Stephen Bau</strong> from Design Influences.
+      </p>
+      <p>Exploring how imagine, design, and build the future together.</p>
+    </div>
+  </div>
+</template>
